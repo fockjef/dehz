71abe13a9e1b4ab913d8c8b1280dfa646c0cb782
jef <fockjef@gmail.com>
2020-08-12 14:51:33 -0500

    Refactors dehz.pl so it's less cryptic. Adds duplicate detection and solutions for extra levels.

--- dehz.pl
+++ dehz.pl
@@ -1,85 +1,154 @@
 #!/usr/bin/perl
 
+# default JSON::XS module tends to treat numbers as strings
+# export PERL_JSON_BACKEND=JSON::PP
+
 use strict;
 use JSON;
+use Data::Dumper;
 
-chdir $ARGV[0];
-opendir my $DIR, ".";
-my @Files = sort readdir $DIR;
+opendir my $DIR, $ARGV[0];
+my @Files = map {$ARGV[0]."/".$_} readdir $DIR;
 closedir $DIR;
 
-my $dehz = {};
-
-foreach my $f (grep {-f $_} @Files){
-	my $data = do{local $/;open my $IN, $f;binmode $IN;<$IN>} =~ /(level[\w\W]*)/ ? $1 : "";
-	if( $data =~ /^level_(?<type>pack)s_(?<id>\d+)\0*....(?<data>\{[\w\W]*\})/ || $data =~ /^level(?<id>\d+)(?<type>Solution)\0*....(?<data>\{[\w\W]*\})/ || $data =~ /^(?<type>level)(?<id>\d+)\0*....(?<data>\{[\w\W]*\})/ ){
-		$dehz->{lc $+{type}}{$+{id}} = from_json($+{data});
-	}
-	else{
-		$data = substr($data,0,100);
-		$data =~ s/([\x00-\x1f\x7f-\xff])/?/g;
-		print STDERR "$f\t$data\n";
-	}
-}
-
-delete $dehz->{level}{"00"};
-foreach my $l (keys %{$dehz->{level}}){
-	my $s = $dehz->{solution}{$l}{solution};
-	$l = $dehz->{level}{$l};
-	$l->{cols} = $l->{columns};
-	foreach my $t (@{$l->{goals}},@{$l->{rangeElements}}){
-		push @{$l->{tiles}}, {x=>$t->{position}{x},y=>$t->{position}{y},range=>$t->{range}||-1};
-	}
-	delete @{$l}{qw/columns goals rangeElements/};
-	if( $s ){
-		foreach my $m (@$s){
-			$m = {
-				x =>  $m->{rangeElements}{position}{x},
-				y =>  $m->{rangeElements}{position}{y},
-				dx => $m->{direction}{x},
-				dy => $m->{direction}{y}
-			};
-		}
-		$l->{solution} = $s;
-	}
-}
-
-my ($levelOffset,@packs,@levels);
-foreach my $p (sort {$a<=>$b} keys %{$dehz->{pack}}){
-	$p = $dehz->{pack}{$p};
-	$p->{firstLevel} = $levelOffset+1;
-	foreach my $l (@{$p->{levels}}){
-		$l =~ s/level//;
-		if( exists $dehz->{level}{$l} ){
-			$dehz->{level}{$l}{packId} = $p->{packId};
-			push @levels, delete $dehz->{level}{$l};
-			$levelOffset++;
+my $data = {};
+foreach my $file (grep {-f $_} @Files){
+	# File format appears to be ~64 bytes header then null pad up to byte 4096
+	# followed by an arbitrary series of blocks where the first 4 bytes are
+	# the length of the block-4** and then actual block data
+	# **blocks are padded to be multiples of 4 bytes
+	# Check that byte 3 is 0x49 ("I") and byte 37 is \x31 ("1")
+	# This seems to indicate the file has JSON data where the first block is a
+	# description and the second block is the actual JSON data
+	open my $IN, $file;
+	binmode $IN;
+	my $buffer;
+	seek $IN, 3, 0;
+	read $IN, $buffer, 1;
+	if( $buffer eq "\x49" ){
+		seek $IN, 37, 0;
+		read $IN, $buffer, 1;
+		if( $buffer eq "\x31" ){
+			seek $IN, 4096, 0;
+			read $IN, $buffer, 4;
+			my $len = unpack "L", $buffer; # length of description
+			read $IN, my $desc, $len;
+			if( $len % 4 ){
+				seek $IN, 4-($len%4), 1; # skip to start of next 4-byte block
+			}
+			read $IN, $buffer, 4;
+			$len = unpack "L", $buffer; # length of JSON data
+			if( $len ){
+				read $IN, my $json, $len;
+				$data->{$desc} = eval{ decode_json($json) };
+				die "ERROR: Invalid JSON data ($file)" if $@;
+				$data->{$desc}{file} = $file;
+			}
 		}
 	}
-	if( $levelOffset >= $p->{firstLevel} ){
-		$p->{c1} = $p->{colorPrimaryPack};
-		$p->{c2} = $p->{colorSecundaryPack};
-		$p->{lastLevel} = $levelOffset;
-		delete @{$p}{qw/colorPrimaryPack colorSecundaryPack levels/};
-		push @packs, $p;
+}
+
+# delete garbage level
+delete $data->{level00};
+
+# Level descriptions are in the form "levelN" where N is the level number 
+foreach my $desc (grep {/^level\d+$/} keys %{$data}){
+	# read board layout
+	my (@tiles);
+	my ($minX,$maxX,$minY,$maxY) = (0xff,-1,0xff,-1);
+	foreach my $tile (@{$data->{$desc}{rangeElements}},@{$data->{$desc}{goals}}){
+		# determine tile boundaries for generating level fingerprint
+		$minX = $tile->{position}{x} if $tile->{position}{x} < $minX;
+		$maxX = $tile->{position}{x} if $tile->{position}{x} > $maxX;
+		$minY = $tile->{position}{y} if $tile->{position}{y} < $minY;
+		$maxY = $tile->{position}{y} if $tile->{position}{y} > $maxY;
+		push @tiles, {
+			x => $tile->{position}{x},
+			y => $tile->{position}{y},
+			range => $tile->{range} || -1
+		};
+	}
+	$data->{$desc} = {
+		file => (split(/\//,$data->{$desc}{file}))[-1],
+		fp => join( ":", $maxX-$minX+1, $maxY-$minY+1, map {($_->{x}-$minX,$_->{y}-$minY,$_->{range})} sort {$a->{x}<=>$b->{x}||$a->{y}<=>$b->{y}} @tiles),
+		cols => $data->{$desc}{columns},
+		rows => $data->{$desc}{rows},
+		tiles => \@tiles
+	};
+}
+
+# Solution descriptions are in the form "levelNSolution" where N is the level number 
+foreach my $desc (grep {/^level\d+Solution$/} keys %{$data}){
+	# read solution moves (these are the hints)
+	my @solution;
+	foreach my $move (@{$data->{$desc}{solution}}){
+		push @solution, {
+			x  => $move->{rangeElements}{position}{x},
+			y  => $move->{rangeElements}{position}{y},
+			dx => $move->{direction}{x},
+			dy => $move->{direction}{y}
+		}
+	}
+	$data->{substr($desc,0,-8)}{solution} = \@solution;
+}
+# Load extra solutions
+if( -e "solutions.json" ){
+	my $solutions = eval{ decode_json(do{undef $/;open my $IN, "solutions.json";<$IN>}) };
+	die "ERROR: Invalid JSON data (solutions.json)" if $@;
+	foreach my $levelId (keys %{$solutions}){
+		if( exists $data->{$levelId} ){
+			$data->{$levelId}{solution} = $solutions->{$levelId};
+		}
 	}
 }
 
-my $p = {
-	firstLevel=>$levelOffset+1,
-	packId=>scalar(@packs)
-};
-foreach my $l (keys %{$dehz->{level}}){
-	$l = $dehz->{level}{$l};
-	$l->{packId} = $p->{packId};
-	push @levels, $l;
-	$levelOffset++;
-}
-if( $levelOffset >= $p->{firstLevel} ){
-	$p->{c1} = $packs[0]->{c2};
-	$p->{c2} = $packs[0]->{c1};
-	$p->{lastLevel} = $levelOffset;
-	push @packs, $p;
+my (@Levels,@Packs,%FingerPrints);
+my $lastLevel = 0;
+# Level pack descriptions are in the form "level_packs_N" where N is the pack number
+foreach my $desc (sort {$data->{$a}{packId}<=>$data->{$b}{packId}} grep {/^level_packs_\d+$/} keys %{$data}){
+	my $numLevels = 0;
+	# Add levels to @Levels based on pack order
+	foreach my $level (@{$data->{$desc}{levels}}){
+		if( exists $data->{$level} ){
+			$data->{$level}{packId} = $data->{$desc}{packId}; # add packId property to the level
+			push @Levels, $data->{$level};
+			$FingerPrints{$data->{$level}{fp}} = sprintf "%s (%s)", $level, $data->{$level}{file}; # add fingerprint for duplicate level detection
+			$numLevels++;
+			delete $data->{$level};
+		}
+	}
+	if( $numLevels ){
+		$Packs[$data->{$desc}{packId}] = {
+			packId => $data->{$desc}{packId},
+			c1 => $data->{$desc}{colorPrimaryPack},
+			c2 => $data->{$desc}{colorSecundaryPack},
+			firstLevel => $lastLevel+1,
+			lastLevel => $lastLevel+$numLevels
+		};
+		$lastLevel += $numLevels;
+	}
 }
 
-print to_json({packs=>\@packs,levels=>\@levels},{canonical=>1});
+# Add extra levels and an extra level pack
+my @extraLevels = sort {substr($a,5)<=>substr($b,5)} grep {/^level\d+$/ && !exists $FingerPrints{$data->{$_}{fp}}} keys %{$data};
+if( scalar(@extraLevels) ){
+	push @Levels, map {$data->{$_}{packId}=scalar(@Packs);$data->{$_}} @extraLevels;
+	push @Packs, {
+		packId => $Packs[-1]{packId}+1,
+		c1 => $Packs[-1]{c1},
+		c2 => $Packs[-1]{c2},
+		firstLevel => $lastLevel+1,
+		lastLevel => $lastLevel+scalar(@extraLevels)
+	};
+}
+# Report duplicate levels
+my @duplicateLevels = sort {substr($a,5)<=>substr($b,5)} grep {/^level\d+$/ && exists $FingerPrints{$data->{$_}{fp}}} keys %{$data};
+foreach my $level (@duplicateLevels){
+	printf STDERR "ERROR: Duplicate Level - %s (%s) is identical to %s\n", $level, $data->{$level}{file}, $FingerPrints{$data->{$level}{fp}};
+}
+
+# Remove unnecessary properties
+map {delete @{$_}{qw/file fp levelId/}} @Levels;
+
+print to_json({packs=>\@Packs,levels=>\@Levels},{canonical=>1});
+exit;
